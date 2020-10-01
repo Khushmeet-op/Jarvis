6c145fe32e8d2826d0543a6d770a5e0650c54d81
neerajregmi10 <62173618+neerajregmi10@users.noreply.github.com>
2020-10-01 19:48:55 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,6 +11,6 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`Jarvis at your service ^.^ \n`"
+    await alive.edit("`Jarvis at your service ^.^ \n\n`"
                      f" {DEFAULTUSER} Hey I'm Alive :) ")
                       
