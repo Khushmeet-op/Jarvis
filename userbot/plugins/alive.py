c4e550c5a5851812c94f11e410023929af50d3c6
neerajregmi10 <62173618+neerajregmi10@users.noreply.github.com>
2020-10-01 19:45:04 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,8 +11,6 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`Jarvis at your service ^.^ \nYour bot is running\n\nTelethon version: 6.9.0\nPython: 3.7.3\n\n`"
-                     f"`My peru owner`: {DEFAULTUSER}\n"
-                     "`Telethon version: 6.9.0\nPython: 3.7.3\n`\n"
-                     "`Database Status: Databases functioning normally!\nAlways with you, my master!`\n@Evilvenom2006 `is Gay`..."
-                     "")
+    await alive.edit("`Jarvis at your service ^.^ \n`"
+                     f" {DEFAULTUSER} Hey I'm Alive :) ")
+                      
